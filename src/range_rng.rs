26b5366d311de2627a8078d80de807d20530fa71
Scott Munro <scottnmunro@gmail.com>
2021-02-13 15:48:01 -0800

    add support for select_rand

--- src/range_rng.rs
+++ src/range_rng.rs
@@ -4,6 +4,11 @@ pub trait RangeRng<T: PartialOrd> {
     fn gen_range(&mut self, lower: T, upper: T) -> T;
 }
 
+pub fn select_rand<'a, T>(seq: &'a [T], rng: &mut dyn RangeRng<usize>) -> &'a T {
+    let index = rng.gen_range(0, seq.len());
+    &seq[index]
+}
+
 pub struct ThreadRangeRng {
     rng: rand::rngs::ThreadRng,
 }
@@ -25,6 +30,7 @@ impl<T: PartialOrd + rand::distributions::uniform::SampleUniform> RangeRng<T> fo
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::mocks;
     use crate::test_utils::sample_gen_range_caller;
 
     #[test]
@@ -34,5 +40,12 @@ mod tests {
         let first_value = rng.gen_range(0, 10);
         let next_value = sample_gen_range_caller(&mut rng, 10, 20);
         assert_ne!(first_value, next_value);
+    }
+
+    #[test]
+    fn test_select_rand() {
+        let mut rng = mocks::SingleValueRangeRng::new(2usize);
+        let elements = ["a", "b", "c", "d"];
+        assert_eq!(select_rand(&elements, &mut rng), &"c");
     }
 }
